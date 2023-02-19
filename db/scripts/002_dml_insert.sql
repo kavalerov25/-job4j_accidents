d2500ff408361ef341f7c4920e1286a39a8f4505
kavalerov25 <paradise_777@mail.ru>
2023-02-19 18:18:18 +0300

    1. Spring mock [#2096]

--- db/scripts/002_dml_insert.sql
+++ db/scripts/002_dml_insert.sql
@@ -10,3 +10,8 @@ INSERT INTO accident (name, text, address, type_id) VALUES ('Name 1', 'Descripti
 INSERT INTO accident (name, text, address, type_id) VALUES ('Name 2', 'Description 2', 'Address 2', 2);
 INSERT INTO accident (name, text, address, type_id) VALUES ('Name 3', 'Description 3', 'Address 3', 3);
 
+INSERT INTO accident_rule (accident_id, rule_id) VALUES (1, 1);
+INSERT INTO accident_rule (accident_id, rule_id) VALUES (1, 3);
+INSERT INTO accident_rule (accident_id, rule_id) VALUES (2, 2);
+INSERT INTO accident_rule (accident_id, rule_id) VALUES (2, 3);
+INSERT INTO accident_rule (accident_id, rule_id) VALUES (3, 3);
