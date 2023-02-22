e479633001bca7fb672e0fd137483d9c08c3dafc
kavalerov25 <paradise_777@mail.ru>
2023-02-22 22:20:58 +0300

    1. Spring mock [#2096] убрал лишние конфиги

--- src/test/java/ru/job4j/controller/RegControlTest.java
+++ src/test/java/ru/job4j/controller/RegControlTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.controller;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,15 +8,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.Main;
-import ru.job4j.model.User;
 import ru.job4j.service.AuthorityService;
 import ru.job4j.service.UserService;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -52,29 +46,5 @@ class RegControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
-    }
-
-    @Test
-    @WithMockUser
-    public void whenUserRegistered() throws Exception {
-        User user = new User("User", "123456");
-        mockMvc.perform(post("/reg")
-                        .flashAttr("user", user))
-                .andDo(print())
-                .andExpect(status().is3xxRedirection());
-        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
-        verify(service).save(argument.capture());
-        assertThat(argument.getValue().getUsername(), is("User"));
-    }
-
-    @Test
-    @WithMockUser
-    public void whenUserWithSameLoginAlreadyExists() throws Exception {
-        User user = new User("User", "123456");
-        service.save(new User("User", "654321"));
-        mockMvc.perform(post("/reg")
-                        .flashAttr("user", user))
-                .andDo(print())
-                .andExpect(status().is(302));
     }
 }
